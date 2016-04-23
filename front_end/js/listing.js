c910411d7efd8a76d9bca460914dfc9a675523a2
Jguer <joaogg3@gmail.com>
2016-04-23 23:53:18 +0100

    Gets have arrived. Frontend-backend communication is established

--- front_end/js/listing.js
+++ front_end/js/listing.js
@@ -7,8 +7,13 @@ $('.category-button').each( function(index) {
   $(this).click( function() {
     this_text = $(this).find("p").text();
     console.log("Nome da categoria: " + this_text);
-    $( '.button_container' ).slideUp( "fast", function() {
+    $( '.button-container' ).slideUp( "fast", function() {
     // Animation complete.
+    });
+
+    $.get( "http://172.17.108.101:9000/stories", { theme: "cultura"} )
+  .done(function( data ) {
+    console.log( "Data Loaded: " + data );
     });
 
     $('.thread-container_text').text(this_text);
@@ -19,7 +24,7 @@ $('.category-button').each( function(index) {
 
 $( '.back-button' ).click(function() {
   $('.thread-container').addClass("hidden");
-  $('.button_container').slideDown( "fast", function(){
+  $('.button-container').slideDown( "fast", function(){
   });
 
 });
